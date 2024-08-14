c45721fd5596919716ef5e1ba3ca3186c96691a8
Rahul Kumar <76811033+rahulchoudhary7@users.noreply.github.com>
2024-08-14 12:56:13 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -70,7 +70,7 @@ export const Home = () => {
                         <h1 className='text-3xl md:text-5xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-medium'>
                            a passionate
                         </h1>
-                        <div className='text-5xl md:text-8xl font-medium mx-auto text-neutral-400 mb-5'>
+                        <div className='text-5xl md:text-8xl font-medium min-h-5rem mx-auto text-neutral-400 mb-5'>
                            {currentText || ' '}
                         </div>
                      </div>
